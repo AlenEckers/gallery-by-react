2fe48acbeec0d6e728ff71b4c74cc7822a968cf6
huangtao <13258315701@163.com>
2018-01-08 14:53:02 +0800

    section create

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,17 +6,21 @@ import React from 'react';
 let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
-  render() {
-    return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<section className="stage">
+				<section className="img-sec">
+				
+				</section>
+				
+				<nav className="controller-nav">
+				
+				</nav>
+			</section>
+		);
+	}
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
